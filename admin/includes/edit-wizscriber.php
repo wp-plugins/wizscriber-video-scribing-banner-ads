caded5ef87e3e9478b309cb6e5ca36d74dfbb05b
socratous139 <socratous139@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-28 11:15:23 +0000

    fixes
    git-svn-id: https://plugins.svn.wordpress.org/wizscriber-video-scribing-banner-ads/tags/1.22@922401 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/includes/edit-wizscriber.php
@@ -0,0 +1,296 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) // don't load directly
+    die( '-1' );
+?>
+
+<div class="wrap">
+    <?php screen_icon(); ?>
+    <h2><?php
+        echo esc_html( __( 'Add New WizScriber', 'wsvsba' ) );
+
+        if (!$post->initial ) {
+            $new_url = admin_url('admin.php?page=wsvsba&post=new');
+            echo ' <a href="'. $new_url.'" class="add-new-h2">' . esc_html(__('Add New', 'gc')) . '</a>';
+        }
+    ?></h2>
+
+    <?php do_action( 'wizscriber_admin_notices' ); ?>
+
+    <br class="clear" />
+
+    <?php
+    if ( $post ) :
+        if ( current_user_can( 'publish_pages', $post_id ) )
+            $disabled = '';
+        else
+            $disabled = ' disabled="disabled"';
+    ?>
+
+    <form method="post" action="<?php echo esc_url( add_query_arg( array( 'post' => $post_id ), menu_page_url( 'wsvsba', false ) ) ); ?>">
+        <?php if ( current_user_can( 'publish_pages', $post_id ) ) wp_nonce_field( 'wsvsba-save-wizscriber_' . $post_id ); ?>
+        <input type="hidden" id="post_ID" name="post_ID" value="<?php echo (int) $post_id; ?>" />
+        <input type="hidden" id="hiddenaction" name="action" value="save" />
+
+        <div id="poststuff" class="metabox-holder">
+
+            <div id="formleft">
+                <input type="text" id="wsvsba-title" name="wsvsba-title" size="40" value="<?php echo esc_attr( $post->title ); ?>"<?php echo $disabled; ?> />
+
+                <?php if (!$post->initial) : ?>
+                    <p class="tagcode">
+                        Copy Code and paste it in page in which you want to appear &rarr;<br />
+                        <input type="text" id="wizscriber-anchor-text" onfocus="this.select();" readonly="readonly" />
+                    </p>
+                <?php endif; ?>
+                    
+                    
+                <?php 
+                do_meta_boxes( null, 'wizScriber Settings', $post );
+
+                wp_nonce_field('closedpostboxes', 'closedpostboxesnonce', false );
+                wp_nonce_field('meta-box-order', 'meta-box-order-nonce', false );
+                ?>
+                
+                <?php if (current_user_can( 'publish_pages', $post_id)) : ?>
+                <div class="save-wizscriber">
+                    <input type="submit" class="button-primary" name="wsvsba-save" value="Save" />
+                </div>
+                <?php endif; ?>
+            </div>
+            
+            <div id = "formright">
+                <?php    echo "<h2 style = 'margin-left:100px;'>" . __( 'wizScriber Live Preview', 'wizScribe_trdom' ) . "</h2>"; ?> <br />
+                <?php    echo "<h4 style = 'margin-left:95px;'>" . __( 'Check how animation will look on page.', 'wizScribe_trdom' ) . "</h4>"; ?>
+                        <br /> 
+                <?php 
+
+                        $id = (int)$post_id;
+
+                        if ( !$wsvsba_wizscriber = wsvsba_wizscriber($id) ) 
+                            return "";
+                        else
+                            $wsvsba_wizscriber = wsvsba_wizscriber($id);
+
+
+                        $firsttext = $wsvsba_wizscriber->wizscriber_firsttext;
+                        $secondtext = $wsvsba_wizscriber->wizscriber_secondtext;
+                        
+                        $actiononclick = $wsvsba_wizscriber->wizscriber_actiononclick;
+                        $finaltexttop = $wsvsba_wizscriber->wizscriber_finaltexttop;
+                        $finaltextbottom = $wsvsba_wizscriber->wizscriber_finaltextbottom;
+                        $actiononclickurl =$wsvsba_wizscriber->wizscriber_actiononclickurl;
+                        $whentoappear = $wsvsba_wizscriber->wizscriber_whentoappear;
+                        $position = $wsvsba_wizscriber->wizscriber_position;
+
+                        if($position == "right"){
+                            $positionstyle = "margin-left: auto;left: auto;right: 0px;";
+                        }else if($position == "left"){
+                            $positionstyle = "margin-right: auto;right: auto;left: 0px;";
+                        }else if($position =="middle"){
+                            $positionstyle = "margin-right: auto;right: auto;left: auto;margin-left:auto;";
+                        }   
+                ?>
+
+                                <script type="text/javascript">
+
+                                videojs.options.flash.swf = "<?php echo plugins_url()."/wizscriber-video-scribing-banner-ads/wizScribeSchell/" ?>player/video-js.swf"
+
+                                function onclickplay(){
+                                    wizSriberPlayer.play();
+                                }
+
+                                function startPlay(){
+
+                                    wizSriberPlayer.play();
+
+                                }
+
+                                var time = parseInt("<?php echo $whentoappear; ?>") * 1000;
+
+
+
+                                function buttonclickstartprev(){
+
+                                setTimeout(function() {startPlay() }, time);
+                                jQuery('#wizScribe-wrapper').show();
+
+
+                                }
+
+                                jQuery(document).ready(function(){
+
+                                jQuery(function() {
+
+                                jQuery('#wizScribe-wrapper').hover(function() { 
+
+                                    jQuery('#controls-hand').show(); 
+
+                                }, function() { 
+
+                                    jQuery('#controls-hand').hide(); 
+
+                                });
+
+                                });
+
+                                 jQuery('#hand-close').click(function(){
+
+                                    wizSriberPlayer.dispose();
+                                    jQuery('#wizScribe-wrapper').hide();
+
+                                 });
+
+                                 jQuery('#hand-mute').click(function(){
+
+                                    wizSriberPlayer.muted(true);
+
+                                 });
+
+                                 jQuery('#hand-play').click(function(){
+                                        i = 0;
+                                    wizSriberPlayer.pause();
+                                    wizSriberPlayer.load();
+
+                                 });
+
+                                });
+
+                                </script>
+                                <div id = "wizScribe-wrapper">
+                                    
+                                <?php 
+                                $firsttext_arr = explode("//", $firsttext);
+                                $secondtext_arr = explode("//", $secondtext);
+                                $mes_num = max(count($firsttext_arr), count($secondtext_arr));
+                                
+                                for ($x=1; $x<=$mes_num; $x++)
+                                     { $y=$x-1;
+                                ?>
+                                <div class = "firsttext" ><?php echo $firsttext_arr[$y]; ?></div>
+                                <div class = "secondtext" ><?php echo $secondtext_arr[$y]; ?></div>
+                                <?php 
+                                     }
+                                ?>
+                                <div id = "finaltexttop" ><?php echo $finaltexttop; ?></div>
+                                <img src="<?php echo plugins_url()."/wizscriber-video-scribing-banner-ads/" ?>img/Paper.png" alt="Paper" id = "paper" />
+                                <div id = "finaltextbottom" ><?php if($actiononclick == 1){echo '<a id = "finaltextbottoma" href "'.$actiononclickurl.'">';} echo $finaltextbottom; if($actiononclick == 1){echo '</a>'; }?></div>
+                                <img src="<?php echo plugins_url()."/wizscriber-video-scribing-banner-ads/wizScribeSchell/" ?>img/coverimg.png" alt="Call Now" id = "covertxtshow" />
+                                <div style = "border:none;width:384px;background:transparent;">
+                                <div id = "controls-hand">
+
+                                <div id = "crtwrp">
+                                <img src="<?php echo plugins_url()."/wizscriber-video-scribing-banner-ads/" ?>img/speaker-mute.png" alt="Logo" id = "hand-mute" />
+                                <img src="<?php echo plugins_url()."/wizscriber-video-scribing-banner-ads/" ?>img/close.png" alt="Logo" id = "hand-close"/>
+                                <img src="<?php echo plugins_url()."/wizscriber-video-scribing-banner-ads/" ?>img/play-icon.png" alt="Play" id = "hand-play" />
+                                </div>
+                                <a href="http://www.wizmotions.com/" id = "hand-logo-a"><img src="<?php echo plugins_url()."/wizscriber-video-scribing-banner-ads/" ?>img/logo.png" alt="Logo" id = "hand-logo"/></a>
+                                </div>
+
+                                <video id="wizScriber" style = "z-index:-1;" width="384" height="320" data-setup='{"example_option":true}'>
+
+                                 <source src="<?php echo plugins_url()."/wizscriber-video-scribing-banner-ads/wizScribeSchell/" ?>Hand-knocking_v4.flv" type = "video/x-flv" />
+
+                                </video>
+
+                                </div>
+                                </div>
+                                <script>
+
+                                var wizSriberPlayer = videojs('wizScriber');
+                                var mes_num = parseInt("<?php echo $mes_num; ?>") - 1;
+                                                               
+                                wizSriberPlayer.on("ended", function(){ 
+
+                                    (document).getElementById("covertxtshow").style.display = "block";
+                                    (document).getElementById("finaltexttop").style.display = "block";
+                                    (document).getElementById("finaltextbottom").style.display = "block";
+                                    (document).getElementById("paper").style.marginTop = '280px';
+                                    jQuery(".firsttext").hide().animate({width:'0px'}, 100);
+                                    jQuery(".secondtext").hide().animate({width:'0px'}, 100);
+
+                                });
+                                
+                                var i=0;
+                                function onTime(){
+                                    
+                                    if (wizSriberPlayer.currentTime() > 16) {
+                                            
+                                        jQuery(".firsttext").each(function(){
+                                            
+                                             jQuery(this).css("display", "none");
+                                             jQuery(this).css("width", "0px");  
+                                            
+                                         });
+                                        
+                                        jQuery(".secondtext").each(function(){
+                                          
+                                            jQuery(this).css("display", "none");
+                                            jQuery(this).css("width", "0px");  
+                                          
+                                        });  
+                                        
+                                    }
+                                    
+                                    
+                                    if (wizSriberPlayer.currentTime() > 15.5 && i<mes_num) {
+                                            
+                                        jQuery(".firsttext").each(function(index){
+                                            if (index==(i)){
+                                             jQuery(this).css("display", "none");  
+                                            } 
+                                         });
+                                        
+                                        jQuery(".secondtext").each(function(index){
+                                           if (index==(i)){
+                                            jQuery(this).css("display", "none"); 
+                                           } 
+                                        });  
+                                        
+                                        wizSriberPlayer.currentTime(7); 
+                                        i=i+1;
+                                    }
+
+                                    if (wizSriberPlayer.currentTime() > 7.5 && wizSriberPlayer.currentTime() < 8) {
+                                        jQuery(".firsttext").each(function(index){
+                                           if (index==i){
+                                            jQuery(this).show().animate({width:'330px'}, 2900);     
+                                           } 
+                                        });
+                                    }
+
+                                    if (wizSriberPlayer.currentTime() > 5.8 && wizSriberPlayer.currentTime() < 7) {
+
+                                            jQuery("#paper").animate({marginTop:'-40px'}, 580);
+
+                                    }
+
+                                    if (wizSriberPlayer.currentTime() > 10.2 && wizSriberPlayer.currentTime() < 11) {
+                                        jQuery(".secondtext").each(function(index){
+                                           if (index==i){
+                                            jQuery(this).show().animate({width:'330px'}, 2900);     
+                                           } 
+                                        });
+                                    }
+
+                                }
+
+                                setInterval(function(){onTime()}, 100);
+
+                                wizSriberPlayer.on("play", function(){ 
+
+                                    (document).getElementById("covertxtshow").style.display = "none";
+                                    (document).getElementById("finaltexttop").style.display = "none";
+                                    (document).getElementById("finaltextbottom").style.display = "none";
+
+                                });
+
+                                </script>	
+
+                        <input type="button" name="Submit" style = "margin-left:50px;margin-top:30px;" value="<?php _e('Preview', 'wizScribe_trdom' ) ?>" id = "idstartprev" onclick = "buttonclickstartprev()" class = "button-primary revblue"/>  
+            </div>
+        
+        </div>
+    </form>
+    <?php endif; ?>
+</div>
---
+++ classes/class-wizScriber.php
@@ -0,0 +1,136 @@
+<?php
+if(!class_exists('WSVSBA_wizScriber')):
+        class WSVSBA_wizScriber {
+
+            public static $found_items = 0;
+            var $initial = false;
+            var $id;
+            var $title;
+
+            public static function register_post_type() {
+                if (post_type_exists(WSVSBA_POSTTYPE))
+                    return;
+
+                register_post_type(WSVSBA_POSTTYPE, array(
+                    'labels' => array(
+                        'name' => __('wizScribers', 'imw'),
+                        'singular_name' => __('wizScriber', 'imw')),
+                    'rewrite' => false,
+                    'query_var' => false));
+            }
+
+            public static function find($args = '') {
+                $defaults = array(
+                    'post_status' => 'any',
+                    'posts_per_page' => -1,
+                    'offset' => 0,
+                    'orderby' => 'ID',
+                    'order' => 'ASC');
+
+                $args = wp_parse_args($args, $defaults);
+
+                $args['post_type'] = WSVSBA_POSTTYPE;
+
+                $q = new WP_Query();
+                $posts = $q->query($args);
+
+                self::$found_items = $q->found_posts;
+
+                $objs = array();
+
+                foreach ((array) $posts as $post)
+                    $objs[] = new self($post);
+
+                return $objs;
+            }
+
+            public function __construct($post = null) {
+                $this->initial = true;
+
+                $post = get_post($post);
+
+                if ($post && WSVSBA_POSTTYPE == get_post_type($post)) {
+                    $this->initial = false;
+                    $this->id = $post->ID;
+                    $this->title = $post->post_title;
+
+                    $props = $this->get_properties();
+
+                    foreach ($props as $prop => $value) {
+                        if (metadata_exists('post', $post->ID, '_' . $prop))
+                            $this->{$prop} = get_post_meta($post->ID, '_' . $prop, true);
+                        else
+                            $this->{$prop} = get_post_meta($post->ID, $prop, true);
+                    }
+                }
+            }
+
+            function get_properties() {
+                $prop_names = array('wizscriber_firsttext', 'wizscriber_secondtext', 'wizscriber_actiononclick', 'wizscriber_finaltexttop', 'wizscriber_finaltextbottom', 'wizscriber_actiononclickurl',  'wizscriber_whentoappear', 'wizscriber_position');
+                //$prop_names = array('firsttext', 'secondtext', 'actiononclick', 'finaltexttop', 'finaltextbottom', 'actiononclickurl', 'whentoappear', 'position');
+                //$prop_names = array('coupon_desc', 'coupon_url');
+                $properties = array();
+
+                foreach ($prop_names as $prop_name)
+                    $properties[$prop_name] = isset($this->{$prop_name}) ? $this->{$prop_name} : '';
+
+                return $properties;
+            }
+
+            function save() {
+                $props = $this->get_properties();
+
+                $post_content = implode("\n", wsvsba_array_flatten($props));
+
+                if ($this->initial) {
+                    $post_id = wp_insert_post(array(
+                        'post_type' => WSVSBA_POSTTYPE,
+                        'post_status' => 'publish',
+                        'post_title' => $this->title,
+                        'post_content' => trim($post_content)));
+                } else {
+                    $post_id = wp_update_post(array(
+                        'ID' => (int) $this->id,
+                        'post_status' => 'publish',
+                        'post_title' => $this->title,
+                        'post_content' => trim($post_content)));
+                }
+
+                if ($post_id) {
+                    foreach ($props as $prop => $value)
+                        update_post_meta($post_id, '_' . $prop, strip_tags($value));
+
+                    if ($this->initial) {
+                        $this->initial = false;
+                        $this->id = $post_id;
+                    }
+                }
+                return $post_id;
+            }
+
+            function copy() {
+                $new = new WSVSBA_wizScriber();
+                $new->initial = true;
+                $new->title = $this->title . '_copy';
+
+                $props = $this->get_properties();
+
+                foreach ($props as $prop => $value)
+                    $new->{$prop} = $value;
+                return $new;
+            }
+
+            function delete() {
+                if ($this->initial)
+                    return;
+
+                if (wp_delete_post($this->id, true)) {
+                    $this->initial = true;
+                    $this->id = null;
+                    return true;
+                }
+
+                return false;
+            }
+        } //end class
+endif;
